c9441390345e3892f88e82e50d7cebf4389c1017
pspgbhu <brotherchun001@gmail.com>
2017-04-26 16:02:07 +0800

    unit test'

--- test/unit/specs/swipe.spec.js
+++ test/unit/specs/swipe.spec.js
@@ -1,8 +1,33 @@
 import Vue from 'vue';
 import Swipe from 'src/swipe';
+import SwipeItem from 'src/swipe-item';
 
 describe('swipe.vue', () => {
-  it('true be equal true', () => {
-    expect(true).to.be.true;
+  const Ctor = Vue.extend();
+
+  const vm = new Ctor({
+    template: `
+    <div class="">
+      <swipe style="width: 50px; height: 50px;" v-model="value">
+        <swipe-item v-for="i in item" style="">item{{ i }}</swipe-item>
+      </swipe>
+    </div>
+    `,
+
+    data() {
+      return {
+        value: 0,
+        item: [0, 1, 2],
+      };
+    },
+
+    components: {
+      swipe: Swipe,
+      'swipe-item': SwipeItem,
+    },
+  }).$mount();
+
+  it('swipe init status', () => {
+    expect(vm.value).to.be.equal(0);
   });
 });
---
+++ test/unit/units.js
@@ -0,0 +1,2 @@
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'src/main';
